2e0888c7d6c75b44478fa6a9d7473f2ffc4ed75a
Guillaume ADAM <adam.guillaume78@gmail.com>
2022-10-24 23:17:59 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+/* NAVBAR */
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+  $('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+})
+})
+
+
+
+
+/* Fonction " VOIR PLUS "  */
+
+$(document).ready(function(){
+  $(".read").click(function(){
+     $(this).prev().toggle();
+     $(this).siblings('.dots').toggle();
+     if($(this).text()=='Afficher plus'){
+ $(this).text('Afficher moins');
+     }
+     else{
+ $(this).text('Afficher plus');
+     }
+  });
+});
